64c4159e72a114c723a44fa48edac0f3f5133ab2
Lastlilith <lastlilith.2014@gmail.com>
2023-02-11 21:06:41 +0900

    implemented bottom nav

--- app/src/main/java/com/imnidasoftware/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/imnidasoftware/vknewsclient/MainActivity.kt
@@ -3,13 +3,6 @@ package com.imnidasoftware.vknewsclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.imnidasoftware.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,15 +10,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsClientTheme {
-                // A surface container using the 'background' color from the theme
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colors.background)
-                        .padding(8.dp)
-                ) {
-                    PostCard()
-                }
+                MainScreen()
             }
         }
     }
