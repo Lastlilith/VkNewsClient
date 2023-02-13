79fd8b66b7a45886c8c0785be0d3c0adce417f85
Lastlilith <lastlilith.2014@gmail.com>
2023-02-14 00:05:37 +0900

    implemented lazy columns and delete function

--- app/src/main/java/com/imnidasoftware/vknewsclient/VkNewsMainScreen.kt
+++ app/src/main/java/com/imnidasoftware/vknewsclient/VkNewsMainScreen.kt
@@ -1,7 +1,11 @@
 package com.imnidasoftware.vknewsclient
 
-import android.annotation.SuppressLint
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
@@ -10,14 +14,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.imnidasoftware.vknewsclient.domain.FeedPost
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
-
-
-
     Scaffold(
         bottomBar = {
             BottomNavigation {
@@ -47,15 +47,49 @@ fun MainScreen(viewModel: MainViewModel) {
             }
         }
     ) {
-        val feedPost = viewModel.feedPost.observeAsState(FeedPost())
+        val feedPosts = viewModel.feedPosts.observeAsState(listOf())
+        LazyColumn(
+            modifier = Modifier.padding(it),
+            contentPadding = PaddingValues(
+                top = 16.dp,
+                start = 8.dp,
+                end = 8.dp,
+                bottom = 72.dp
+            ),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(
+                items = feedPosts.value,
+                key = { it.id }
+            ) { feedPost ->
+                val dismissState = rememberDismissState()
+                if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+                    viewModel.remove(feedPost)
+                }
 
-        PostCard(
-            modifier = Modifier.padding(8.dp),
-            feedPost = feedPost.value,
-            onViewsClickListener = viewModel::updateCount,
-            onShareClickListener = viewModel::updateCount,
-            onCommentClickListener = viewModel::updateCount,
-            onLikeClickListener = viewModel::updateCount,
-        )
+                SwipeToDismiss(
+                    modifier = Modifier.animateItemPlacement(),
+                    state = dismissState,
+                    background = {},
+                    directions = setOf(DismissDirection.EndToStart)
+                ) {
+                    PostCard(
+                        feedPost = feedPost,
+                        onViewsClickListener = { statisticItem ->
+                            viewModel.updateCount(feedPost, statisticItem)
+                        },
+                        onShareClickListener = { statisticItem ->
+                            viewModel.updateCount(feedPost, statisticItem)
+                        },
+                        onCommentClickListener = { statisticItem ->
+                            viewModel.updateCount(feedPost, statisticItem)
+                        },
+                        onLikeClickListener = { statisticItem ->
+                            viewModel.updateCount(feedPost, statisticItem)
+                        }
+                    )
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/imnidasoftware/vknewsclient/domain/FeedPost.kt
+++ app/src/main/java/com/imnidasoftware/vknewsclient/domain/FeedPost.kt
@@ -3,8 +3,9 @@ package com.imnidasoftware.vknewsclient.domain
 import com.imnidasoftware.vknewsclient.R
 
 data class FeedPost(
+    val id: Int = 0,
     val communityName: String = "/dev/null",
-val publicationDate: String = "14:00",
+    val publicationDate: String = "14:00",
     val avatarResId: Int = R.drawable.post_comunity_thumbnail,
     val contentText: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     val contentImageResId: Int = R.drawable.post_content_image,
